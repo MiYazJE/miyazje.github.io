aaf757caccac094613afab570b51556dbb6b9dbf
MiYazJE <ruben.saizserrano@gmail.com>
2020-06-26 15:55:19 +0200

    New things

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import { proyects } from './proyects.js';
+
 let selectedEl = document.querySelector('.default');
 selectedEl.classList.add('linkSelected')
 
@@ -14,8 +16,22 @@ function setNavLinkSelected(newSelected) {
     selectedEl.classList.add('linkSelected');
 }
 
+function replenishProyects() {
+    const wrapProyects = document.querySelector('#wrapProjects');
+    wrapProyects.innerHTML = proyects.map(proyect => (`
+        <div class="project-card">
+            <img src="${proyect.imagePath}" />
+            <div class="description">
+                <h1>${proyect.name}</h1>
+                <p>${proyect.description}</p>
+                <a target="_blank" href="${proyect.url}">Checkout!</a>
+            </div>
+        </div>`)).join('');
+}
+
 function main() {
     applyEvents();
+    replenishProyects();
 }
 
 main();
---
+++ js/proyects.js
@@ -0,0 +1,22 @@
+const proyects = [
+    {
+        name: 'Pintorrea',
+        description: 'Pintorrea is a multiplayer game where you can fun with your friends drawing.',
+        url: 'https://pintorrea.herokuapp.com',
+        imagePath: '../imgs/pintorrea.PNG'
+    },
+    {
+        name: 'Maze Pathfinding',
+        description: 'A pathfinding algorithm that search the best route between 2 points (Dijkstra).',
+        url: 'https://maze-pathfinding.herokuapp.com/',
+        imagePath: '../imgs/pathfinding.PNG'
+    },
+    {
+        name: 'PokeReactive',
+        description: 'PokeReactive is a website for search any pokemon, it uses a public api.',
+        url: 'https://pokereactive.herokuapp.com/',
+        imagePath: '../imgs/pokeReactive.PNG'
+    },
+];
+
+export { proyects };
