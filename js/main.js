9d55480940ed5ec098c665e06ecd2072432ae905
ruben <ruben.saizserrano@gmail.com>
2024-11-25 18:09:35 +0100

    feat(): update projects

--- js/main.js
+++ js/main.js
@@ -1,93 +1,52 @@
 import { proyects } from './proyects.js';
 
 (function () {
-    const triggerScrollElements = [];
-    const nav = document.querySelector('nav');
-    let selectedEl = document.querySelector('.default');
-
-    function setNavLinkSelected(newSelected) {
-        selectedEl.classList.remove('linkSelected');
-        selectedEl = newSelected;
-        selectedEl.classList.add('linkSelected');
-    }
-
-    function replenishProyects() {
-        const wrapProyects = document.querySelector('#wrapProjects');
-        let delay = 0.2;
-        wrapProyects.innerHTML = proyects.map((proyect) => `
-            <div class="project-card" data-delay="${delay += 0.1}">
+  function renderProyects() {
+    const wrapProyects = document.querySelector('#wrapProjects');
+    let delay = 0.2;
+    wrapProyects.innerHTML = proyects
+      .map((proyect) => {
+        const githubAnchor = proyect.githubUrl
+          ? `<a target="_blank" class="githubBtn" href="${proyect.githubUrl}">Github</a>`
+          : '';
+        const urlAnchor = proyect.url
+          ? `<a target="_blank" href="${proyect.url}">Project</a>`
+          : '';
+        return `
+            <div class="project-card" data-delay="${(delay += 0.1)}">
                 <img src="${proyect.imagePath}" />
                 <div class="description">
                     <h1>${proyect.name}</h1>
                     <p>${proyect.description}</p>
                     <div class="btnsProject">
-                        <a target="_blank" class="githubBtn" href="${proyect.githubUrl}">Github</a>
-                        <a target="_blank" href="${proyect.url}">Project</a>
+                        ${githubAnchor}
+                        ${urlAnchor}
                     </div>
                 </div>
-            </div>`).join('');
-    }
+            </div>`;
+      })
+      .join('');
+  }
 
-    function startScrollElements() {
-        const elements = [
-            document.querySelector('#whoAmI'),
-            document.querySelector('#projects'),
-            document.querySelector('#contact'),
-        ];
-        for (let i = 0; i < elements.length; i++) {
-            const current = elements[i];
-            const currentRect = current.getBoundingClientRect();
-            const isLastElement = i === elements.length - 1;
-            let next, nextRect;
-            if (!isLastElement) {
-                next = elements[i + 1];
-                nextRect = next.getBoundingClientRect();
-            }
-            const id = current.getAttribute('id');
-            triggerScrollElements.push({ 
-                id,
-                element: current,
-                min: currentRect.y,
-                max: !isLastElement ? nextRect.y - 1 : 1000000,
-            }); 
+  function main() {
+    $('body').tooltip({ selector: '[data-toggle=tooltip]' });
+    renderProyects();
+
+    ScrollOut({
+      targets: '.transition-text, .project-card, #portfolio, .viewProjects',
+      onShown: (el) => {
+        el.classList.add('animate__animated');
+        el.style.opacity = 1;
+        el.style.animationDelay = `${el.dataset.delay}s`;
+        if (el.classList.contains('project-card')) {
+          el.classList.add('animate__fadeIn');
+        } else {
+          console.log(el.dataset.animationtype);
+          el.classList.add(el.dataset.animationtype);
         }
-        window.addEventListener('scroll', (e) => {
-            [...Object.values(triggerScrollElements)].forEach(({ id, min, max }) => {
-                if (window.scrollY >= min && window.scrollY <= max) {
-                    if (window.scrollY >= 100) {
-                        nav.classList.add('unscale');
-                    }
-                    else {
-                        nav.classList.remove('unscale');
-                    }
-                    const liElement = document.querySelector(`.${id}Li`);
-                    setNavLinkSelected(liElement);
-                }
-            });
-        });
-    }
+      },
+    });
+  }
 
-    function main() {
-        $("body").tooltip({ selector: '[data-toggle=tooltip]' });
-        replenishProyects();
-        startScrollElements();
-
-        ScrollOut({ 
-            targets: '.transition-text, .project-card, #portfolio, .viewProjects',
-            onShown: (el) => {
-                el.classList.add('animate__animated');
-                el.style.opacity = 1;
-                el.style.animationDelay = `${el.dataset.delay}s`;
-                if (el.classList.contains('project-card')) {
-                    el.classList.add('animate__fadeIn');
-                }
-                else {
-                    console.log(el.dataset.animationtype);
-                    el.classList.add(el.dataset.animationtype);
-                }
-            }
-        });
-    }
-
-    main();
+  main();
 })();
--- js/proyects.js
+++ js/proyects.js
@@ -1,32 +1,36 @@
 const proyects = [
-    {
-        name: 'Pintorrea',
-        description: 'Pintorrea is a multiplayer game where you can fun with your friends drawing.',
-        githubUrl: 'https://github.com/MiYazJE/Pintorrea',
-        url: 'https://pintorrea.herokuapp.com',
-        imagePath: '../imgs/pintorrea.PNG'
-    },
-    {
-        name: 'Pathfinding',
-        description: 'A pathfinding algorithm that search the best route between 2 points (Dijkstra).',
-        githubUrl: 'https://github.com/MiYazJE/Pathfinding-Visualization',
-        url: 'https://pathfinding-visualization.vercel.app/',
-        imagePath: '../imgs/pathfinding.PNG'
-    },
-    {
-        name: 'PokeReactive',
-        description: 'PokeReactive is a website for searching any pokemon, it uses a public api.',
-        githubUrl: 'https://github.com/MiYazJE/PokeReact',
-        url: 'https://poke-react-fawn.vercel.app/',
-        imagePath: '../imgs/pokeReactive.PNG'
-    },
-    {
-        name: 'Video Share',
-        description: 'Video Share is a website to view youtube videos synchronously with friends.',
-        githubUrl: 'https://github.com/MiYazJE/Share-Videos',
-        url: 'http://videowithme.herokuapp.com/',
-        imagePath: '../imgs/videoShare.PNG'
-    },
+  {
+    name: 'Puntos recogida',
+    description:
+      'Website to find resource collection points for those affected by the Dana in Valencia.',
+    githubUrl: 'https://github.com/MiYazJE/centros-recogida-alimentos-valencia',
+    url: 'https://puntosrecogidavalencia.org/',
+    imagePath: '../imgs/puntos-recogida.PNG',
+  },
+  {
+    name: 'Video Share',
+    description:
+      'Share Videos is an innovative web application to watch YouTube videos in real-time with others.',
+    githubUrl: 'https://github.com/MiYazJE/Share-Videos',
+    url: 'https://share-videos.vercel.app/',
+    imagePath: '../imgs/video-share.PNG',
+  },
+  {
+    name: 'Pintorrea',
+    description:
+      'Pintorrea is a multiplayer game where you can fun, drawing with your friends in real time.',
+    githubUrl: 'https://github.com/MiYazJE/Pintorrea',
+    url: '',
+    imagePath: '../imgs/pintorrea.PNG',
+  },
+  {
+    name: 'Pathfinding',
+    description:
+      'A pathfinding algorithm that search the best route between 2 points (Dijkstra).',
+    githubUrl: 'https://github.com/MiYazJE/Pathfinding-Visualization',
+    url: 'https://pathfinding-visualization.vercel.app/',
+    imagePath: '../imgs/pathfinding.PNG',
+  },
 ];
 
 export { proyects };
